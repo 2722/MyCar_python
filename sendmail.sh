78fc5a5865e293af2fa37b1e416aa7a187ba2f69
Ninedyz <Ninedyz@gmail.com>
2016-08-25 20:42:24 +0800

    Update sendmail.sh
    你问我支持不支持邮件里嵌入HTML5视频，我说支持
    所以我就做了一点微小的工作
    
    PS 第一次写shell代码…大神们不要吐槽……
    PS2 centos 6 x86测试通过…不过目前貌似只在QQ邮箱网页版看到了效果(;´･ω･)

--- sendmail.sh
+++ sendmail.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 #i can send mail to your email
+
 obj=`date +"%m%d"`"mycar"
-cat /root/pycar/newurl |mailx -s $obj 你的邮箱地址
+
+echo 'From: pycar' > mailbody
+echo 'To: [!YOURNAME] <[!YOURMAILADDRESS]>' >> mailbody
+# echo 'Cc: someone <someone@example.com>,TooYoung <toosimple@naive.com>' >> mailbody
+echo -e 'Subject: '$obj'\nContent-Type: text/html\n' >> mailbody
+
+cat /root/pycar/newurl | grep -v ^$ | while read oneline
+do
+    echo -e '<p><video controls><source src="'$oneline'" type="video/mp4"><a href="'$oneline'">'$oneline'</a></video></p>\n' >> mailbody
+done
+
+sendmail -t < mailbody
