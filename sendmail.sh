d9916a31fd458178efa6ddeaec0938a4f649f9e3
Ninedyz <Ninedyz@gmail.com>
2016-08-25 20:51:25 +0800

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -5,7 +5,7 @@ obj=`date +"%m%d"`"mycar"
 
 echo 'From: pycar' > mailbody
 echo 'To: [!YOURNAME] <[!YOURMAILADDRESS]>' >> mailbody
-# echo 'Cc: someone <someone@example.com>,TooYoung <toosimple@naive.com>' >> mailbody
+# echo 'Cc: someone <someone@example.com>, TooYoung <toosimple@naive.com>' >> mailbody
 echo -e 'Subject: '$obj'\nContent-Type: text/html\n' >> mailbody
 
 cat /root/pycar/newurl | grep -v ^$ | while read oneline
