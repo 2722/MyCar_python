d5dc237ee5311a28679b0dd7e4670f529c44e52f
Ninedyz <Ninedyz@gmail.com>
2017-03-07 17:06:02 +0800

    Add thumbnails
    <video poster="https://media.tumblr.com/tumblr_[*]_frame1.jpg">

--- sendmail_HTML.sh
+++ sendmail_HTML.sh
@@ -16,7 +16,8 @@ do
 	echo -e 'Subject: '$obj'\nContent-Type: text/html\n' >> mailbody
 	cat /root/pycar/newurl | grep -v ^$|sed '/Now URL/d'| while read oneline
 	do
-		echo -e '<p ><video style="width:540px;height:304px" controls><source src="'$oneline'" type="video/mp4"><a href="'$oneline'">'$oneline'</a></video></p>\n' >> mailbody
+		filename=`echo $oneline | grep -P '(?i)tumblr_.*?(?=\.|$)' -o`
+		echo -e '<p ><video style="width:540px;height:304px" poster="https://media.tumblr.com/'$filename'_frame1.jpg" controls><source src="'$oneline'" type="video/mp4"><a href="'$oneline'">'$oneline'</a></video></p>\n' >> mailbody
 	done
 
 	/usr/sbin/sendmail -t < mailbody
